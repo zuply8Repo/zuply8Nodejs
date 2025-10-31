68db323363171d7eb42f63d5ee28320d5ed5db0e
Victor Solorzano <victor.solorzano08@gmail.com>
2025-10-31 12:50:34 +0100

    First commit Zuply in NodeJs arquitechture

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans text-zinc-800">
+      <p>Hello world!</p>
+    </div>
+  );
+}
